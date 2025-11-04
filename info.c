6062886512ef17f6da011ad035b852f668a1dd08
Bryce <vosburgb@msoe.edu>
2025-11-03 18:08:16 -0600

    Finished version

--- info.c
+++ info.c
@@ -1,9 +1,57 @@
-// Name, etc
+/****************************************************
+ * Name: Bryce Vosburg
+ * Assignment: Lab 9 – System Calls
+ * Section: CPE 2600
+ *
+ * info.c – Print system information using POSIX calls
+ ****************************************************/
 
-// info.c
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+#include <sys/sysinfo.h>
+#include <sys/types.h>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
+
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    printf("Current Time (ns): %ld\n", ts.tv_nsec);
+
+    char hostname[256];
+    if(gethostname(hostname, sizeof(hostname)) == 0){
+        printf("Hostname: %s\n", hostname);
+    }
+
+    struct utsname u;
+    if(uname(&u) == 0){
+        if(u.sysname[0] != '\0'){
+            printf("OS Name: %s\n", u.sysname);
+        }
+        if(u.release[0] != '\0'){
+            printf("OS Release: %s\n", u.release);
+        }
+        if(u.version[0] != '\0'){
+            printf("OS Version: %s\n", u.version);
+        }
+        if(u.machine[0] != '\0'){
+            printf("Hardware Type: %s\n", u.machine);
+        }
+    }
+
+    printf("CPU Count: %d\n", get_nprocs());
+
+    long page_size = sysconf(_SC_PAGESIZE);
+    long pages_total = sysconf(_SC_PHYS_PAGES);
+    long pages_free = sysconf(_SC_AVPHYS_PAGES);
+
+    long long total_mem = (long long)page_size * pages_total;
+    long long free_mem  = (long long)page_size * pages_free;
+
+    printf("Total Memory (bytes): %lld\n", total_mem);
+    printf("Free Memory (bytes): %lld\n", free_mem);
 
     return 0;
 }
