7d4ccb4e8b6d626da7446bad197d59c46d48a0b0
Bryce <vosburgb@msoe.edu>
2025-11-03 21:03:39 -0600

    Reduce priority by 10 and sleep for 1.8 seconds

--- pmod.c
+++ pmod.c
@@ -1,9 +1,29 @@
-// Name, etc
+/****************************************************
+ * Name: Bryce Vosburg
+ * Assignment: Lab 9 – System Calls
+ * Section: CPE 2600
+ *
+ * pmod.c – Modify priority and sleep
+ ****************************************************/
 
-// pmod.c
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
 
-int main(int argc, char* argv[])
-{
+int main(void){
+
+    int result = nice(10);
+    if(result == -1){
+        perror("nice");
+    }
+
+    struct timespec ts;
+    ts.tv_sec = 1;
+    ts.tv_nsec = 837272638;
+
+    nanosleep(&ts, NULL);
+
+    printf("Goodbye from pmod\n");
 
     return 0;
 }
